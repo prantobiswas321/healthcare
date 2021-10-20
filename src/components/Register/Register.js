5677fc511c6f0edd901b9f7317d6179f384f3a25
prantobiswas321 <rahulbd321@gmail.com>
2021-10-20 23:24:32 +0600

    finish

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -30,32 +30,12 @@ const Register = () => {
                             {error}
                         </div>
 
-                        <input type="submit" value="Register" className="bg-danger px-3 py-2 rounded-3 text-white" />
+                        <input type="submit" value="Register" className="bg-success px-3 py-2 rounded-3 text-white" />
 
                         <NavLink className="text-decoration-none" to="/login">
                             <p className="pt-5 fw-bold text-dark">If you already have An Account?</p>
                         </NavLink>
                     </form>
-
-                    {/* <form >
-                        <div className="mb-3">
-                            <label htmlhtmlFor="exampleInputEmail1" className="form-label">Email address :</label>
-                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Your email address' required />
-
-                        </div>
-                        <div className="mb-3">
-                            <label htmlhtmlFor="exampleInputPassword1" className="form-label">Password</label>
-                            <input type="password" className="form-control" id="exampleInputPassword1" placeholder='Password' required />
-                        </div>
-
-                        <input type="submit" value="Register" className="bg-danger px-3 py-2 rounded-3 text-white" />
-
-                        <NavLink to="/login">
-                            <p className="pt-5">Already Have An Account?</p>
-                        </NavLink>
-
-                    </form> */}
-
                 </div>
                 <div className="col">
                     <img src={image1} className="img-fluid ms-5" alt='' />
